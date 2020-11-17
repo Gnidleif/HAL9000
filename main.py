98396ea0002606c8205d40c8666d5074e1f357cf
Tim Fielding <gnidleif@gmail.com>
2020-11-17 15:45:30 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import os, discord, sys
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+def run(*args):
+    return
+
+if __name__ == "__main__":
+    run(sys.argv[1:])
